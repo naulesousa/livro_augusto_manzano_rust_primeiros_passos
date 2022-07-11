b32d91f8f2db9713ed9b95dae34b0c07a1c8b9a8
Blacksmith <davenstall@gmail.com>
2022-07-11 20:56:10 -0300

    Exemplo 03 do Capítulo Dois

---
+++ capitulo_02/c02ex03/target/debug/c02ex03.d
@@ -0,0 +1,1 @@
+/home/blacksmith/manzano_rust_primeiros_passos/capitulo_02/c02ex03/target/debug/c02ex03: /home/blacksmith/manzano_rust_primeiros_passos/capitulo_02/c02ex03/src/main.rs
---
+++ capitulo_02/c02ex03/target/debug/deps/c02ex03-08eb9acd148c6d10.d
@@ -0,0 +1,5 @@
+/home/blacksmith/manzano_rust_primeiros_passos/capitulo_02/c02ex03/target/debug/deps/c02ex03-08eb9acd148c6d10: src/main.rs
+
+/home/blacksmith/manzano_rust_primeiros_passos/capitulo_02/c02ex03/target/debug/deps/c02ex03-08eb9acd148c6d10.d: src/main.rs
+
+src/main.rs:
