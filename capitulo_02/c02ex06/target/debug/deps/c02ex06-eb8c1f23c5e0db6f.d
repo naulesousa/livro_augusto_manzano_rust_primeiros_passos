e172914ae24c3a28b2c1b706cf1e9ccc211c1af6
Blacksmith <57499200+Blacksmith-007@users.noreply.github.com>
2022-07-14 09:20:41 -0300

    Delete capitulo_02/c02ex06/target directory

--- capitulo_02/c02ex06/target/debug/deps/c02ex06-eb8c1f23c5e0db6f.d
+++
@@ -1,5 +0,0 @@
-/home/blacksmith/manzano_rust_primeiros_passos/capitulo_02/c02ex06/target/debug/deps/c02ex06-eb8c1f23c5e0db6f: src/main.rs
-
-/home/blacksmith/manzano_rust_primeiros_passos/capitulo_02/c02ex06/target/debug/deps/c02ex06-eb8c1f23c5e0db6f.d: src/main.rs
-
-src/main.rs:
